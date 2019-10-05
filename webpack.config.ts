3d41d2571b821fde6307c1c63b46e9ea7a904dc3
Mahdi Najjarian <mnajarian@gmail.com>
2019-10-05 19:40:31 +0300

    add file-loader

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,29 +1,33 @@
 /* eslint-disable no-undef */
-import path from 'path';
-import webpack from 'webpack';
+import path from "path";
+import webpack from "webpack";
 
 const config: webpack.Configuration = {
-    entry: './src/index.tsx',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'main.bundle.js',
-    },
-    devtool: 'source-map',
-    devServer: {
-        contentBase: path.resolve(__dirname, 'build'),
-        compress: true,
-        port: 3000,
-        stats: 'errors-only',
-    },
-    resolve: {
-        extensions: ['.js', '.json', '.ts', '.tsx'],
-    },
-    module: {
-        rules: [
-            { test: /\.(ts|tsx)$/, loader: 'ts-loader' },
-            { test: /\.(s*)css$/, loader: ['style-loader', 'css-loader', 'sass-loader'] },
-        ],
-    },
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "main.bundle.js"
+  },
+  devtool: "source-map",
+  devServer: {
+    contentBase: path.resolve(__dirname, "build"),
+    compress: true,
+    port: 3000,
+    stats: "errors-only"
+  },
+  resolve: {
+    extensions: [".js", ".json", ".ts", ".tsx"]
+  },
+  module: {
+    rules: [
+      { test: /\.(ts|tsx)$/, loader: "ts-loader" },
+      {
+        test: /\.(s*)css$/,
+        loader: ["style-loader", "css-loader", "sass-loader"]
+      },
+      { test: /\.(jpe?g|png|gif|svg)$/i, loader: "file-loader" }
+    ]
+  }
 };
 
 export default config;
