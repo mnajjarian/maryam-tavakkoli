bc949b923cb31579b8c72696b1e754b8022fb723
Mahdi Najjarian <mnajarian@gmail.com>
2019-10-24 09:48:29 +0300

    add menu bar

--- src/components/Dashboard/nav.tsx
+++ src/components/Dashboard/nav.tsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import classNames from 'classnames';
 
 const Nav = () => {
+  const[toggle, setToggle] = useState(false);
+  const handleToggle = () => setToggle(!toggle);
+
   return (
     <div className="nav">
       <ul className="nav__list">
         <li className="nav__list__item">Posts</li>
         <li className="nav__list__item">Create new</li>
-        <div className="nav__icon">
+        <div className="nav__icon" onClick={handleToggle} >
           <img
             className="nav__icon__image"
             src={require(`../../assets/images/bio-image.jpg`)}
             alt="admin"
           />
+          <ul className={classNames({
+          nav__menu: true,
+          "nav__menu-hide": !toggle
+        })} >
+          <li className="nav__menu__item">Logout</li>
+        </ul>
         </div>
+        
       </ul>
   
     </div>
