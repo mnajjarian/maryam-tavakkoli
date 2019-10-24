5fd7697cfe997968c8cb6ec54642cc12f8461c7b
Mahdi Najjarian <mnajarian@gmail.com>
2019-10-24 12:20:53 +0300

    button component

---
+++ src/components/button.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface ButtonProps {
+    text: string;
+    handleClick: () => void;
+}
+const Button = (props: ButtonProps) => {
+    const { text, handleClick } = props;
+    
+    return(
+        <button type="submit" className="button" onClick={handleClick} >{text}</button>
+    );
+};
+
+export default Button;
