e2f029eea6edac951822f52f6622f12dfe115d7c
Mahdi Najjarian <mnajarian@gmail.com>
2019-10-24 15:34:28 +0300

    preview draftjs

---
+++ src/components/Dashboard/preview.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { EditorState, convertFromRaw, RawDraftContentState } from 'draft-js'
+import { stateToHTML } from 'draft-js-export-html';
+import db from './draft.db.json';
+import renderHTML from 'react-render-html';
+
+const editorContent: RawDraftContentState = db;
+
+const Preview = () => {
+    const content = convertFromRaw(editorContent);
+    const editorState = EditorState.createWithContent(content)
+    const editorContentHtml = stateToHTML(editorState.getCurrentContent())
+    return(
+        <div>
+            {renderHTML(editorContentHtml)}
+        </div>
+    )
+};
+export default Preview;
