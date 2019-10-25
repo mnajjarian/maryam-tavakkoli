639afcd2b544500f3f7f98e784a968e7a404952b
Mahdi Najjarian <mnajarian@gmail.com>
2019-10-25 11:26:46 +0300

    add services

---
+++ src/services/authService.ts
@@ -0,0 +1,15 @@
+import axios from 'axios'
+const baseURL = 'http://localhost:3001';
+
+export const useAuthService = (state: any, dispatch: any) => {
+    const signin = (credentials: any) => {
+        console.log(credentials)
+        axios.post(`${baseURL}/login`, credentials)
+        .then(res => {
+            console.log(res)
+        })
+    }
+    return {
+        signin
+    }
+};
