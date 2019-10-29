d65880ebfa86fc13835e03a383757b7a12608d56
Mahdi Najjarian <mnajarian@gmail.com>
2019-10-29 21:47:08 +0200

    add axios

---
+++ src/services/customAxios.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const baseURL = 'http://localhost:3001'
+
+export const customAxios = axios.create({
+    baseURL
+})
+
+export const setAuthToken = (token: string) => {
+    if(token) {
+        customAxios.defaults.headers.common.Authorization = 'Bearer ${token}'
+    }
+}
