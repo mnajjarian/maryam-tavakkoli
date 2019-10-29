c366a678b4dd3ceec4a81c8daa4b08ae6e36637f
Mahdi Najjarian <mnajarian@gmail.com>
2019-10-29 21:46:12 +0200

    add authContext

---
+++ src/contexts/authContext.tsx
@@ -0,0 +1,20 @@
+import React, { ReactNode, createContext, useReducer } from 'react';
+import { authReducer, initialAuthState, AuthState } from "../reducers/authReducer";
+import { useAuthService } from '../services/authService';
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+export const AuthContext = createContext({} as any);
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [authState, authDispatch] = useReducer(authReducer, initialAuthState);
+  const authService = useAuthService(authState, authDispatch);
+
+  const values = {
+    authState,
+    authDispatch,
+    authService,
+  };
+  return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>
+};
