82aac689bbf5a931e3f38a83fbae1c3f6168849d
Mahdi Najjarian <mnajarian@gmail.com>
2019-10-29 21:57:25 +0200

    add private routers

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,13 +1,44 @@
-import React from "react";
-import { Switch, Route } from "react-router";
+import React, { useContext, useEffect } from "react";
+import { Switch, Route, Redirect } from "react-router";
 import Biography from "./bio";
 import About from "./about";
 import Blog from "./blog";
 import Post from "./post";
-import Login from './login';
-import Dashboard from './Dashboard/dashboard';
+import Login from "./login";
+import Dashboard from "./Dashboard/dashboard";
+import { AuthContext } from "../contexts/authContext";
+import { userInfo } from "os";
+import { DataContext } from "../contexts/dataContext";
 
+interface Props {
+  component: any;
+  path: string;
+}
 const App = () => {
+  const {authState: { isLoggedIn }} = useContext(AuthContext);
+  const { data, dataService } = useContext(DataContext);
+  const fetchAllPosts = () => dataService.getPosts();
+  const fetchProfile = () => dataService.getProfile()
+  useEffect(() => {
+    fetchAllPosts();
+    fetchProfile();
+  },[])
+  const PrivateRoutes = ({
+    component: Comp,
+    path,
+    ...rest
+  }: Props) => (
+    <Route
+      path={path}
+      render={props =>
+        isLoggedIn ? (
+          <Comp {...props} {...rest} />
+        ) : (
+          <Redirect to={{ pathname: "/", state: { from: props.location } }} />
+        )
+      }
+    />
+  );
   return (
     <div>
       <Switch>
@@ -20,8 +51,7 @@ const App = () => {
           path="/blog/:id"
           component={(props: any) => (props ? <Post {...props} /> : null)}
         />
-        <Route exact path='/dashboard' component={Dashboard} />
-
+        <PrivateRoutes path="/dashboard"  component={Dashboard} />
       </Switch>
     </div>
   );
