a9257a509ee1cd42e7038e2957d9728b449aa043
Mahdi Najjarian <mnajarian@gmail.com>
2019-10-29 21:46:52 +0200

    add dataReducer

---
+++ src/reducers/dataReducer.ts
@@ -0,0 +1,39 @@
+import Biography from "../components/bio"
+
+export interface Profile {
+    id: string;
+    name: string;
+    biography: string;
+}
+export interface Blog {
+    id: string;
+    content: string;
+    createdAt: string;
+}
+export interface DataState {
+    blogs: Blog[],
+    profile: Profile | unknown
+}
+export const initialDataState: DataState = {
+    blogs: [],
+    profile: null as unknown,
+}
+export type DataAction = 
+| { type: 'FETCH_PROFILE', payload: any } 
+| { type: 'ADD_POST', payload: Blog } 
+| { type: 'FETCH_POSTS', payload: Blog[] }
+
+
+export const dataReducer = (state: DataState, action: DataAction) => {
+    switch (action.type) {
+        case 'FETCH_PROFILE':
+            return {...state, profile: action.payload };
+        case 'FETCH_POSTS':
+            return {...state, blogs: action.payload }
+        case 'ADD_POST':
+            return {...state, blogs: state.blogs.concat(action.payload)}
+            break;
+        default:
+            return state;
+    }
+}
