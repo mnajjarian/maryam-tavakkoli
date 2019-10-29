af138dda27ab2d413218dfa8c95c35bca20bf647
Mahdi Najjarian <mnajarian@gmail.com>
2019-10-29 21:47:58 +0200

    serve data from server

--- src/components/bio.tsx
+++ src/components/bio.tsx
@@ -1,31 +1,38 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useContext } from "react";
 import Section from "./section";
 import Header from "./header";
 import Footer from "./footer";
+import { DataContext } from "../contexts/dataContext";
+const baseUrl = 'http://localhost:3001/'
 
-const bioImg = require("../assets/images/bio-image.jpg");
 
-const Biography = () => (
+interface Profile {
+  name: string;
+  image: string;
+  biography: string;
+}
+const Biography = () => {
+  const { data: { profile } } = useContext(DataContext);
+
+  if(!profile) {
+    return <div></div>
+  }
+  const { name, image, biography }: Profile = profile[0];
+  return(
   <Fragment>
     <Header />
     <Section
-      imgUrl={bioImg}
-      title="maryam tavakkoli"
+      imgUrl={baseUrl+image}
+      title={name}
       href="/about"
       btnText="read more"
       flexDirect="row"
       borderRadius="50%"
-      text='I am a second-year master student in "Cloud computing and services"
-    with minors in "Entrepreneurship and business". I am doing my
-    studies in “EIT Digital Master School”, where I will obtain a
-    double-degree from two universities which I have studied in: Aalto
-    University in Finland, and TU Delft in the Netherlands. Currently, I
-    am working at Nokia Bell Labs, in Finland, as a thesis writer. In
-    this position, I am conducting a research study on containerizing of
-    a current telco IoT publish/subscribe application...'
+      text={biography}
     />
     <Footer/>
   </Fragment>
-);
+)
+};
 
 export default Biography;
