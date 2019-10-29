af5d041772e02a9e49f8ba26806ae5a5a27c8312
Mahdi Najjarian <mnajarian@gmail.com>
2019-10-29 21:47:29 +0200

    add dataService

---
+++ src/services/dataService.ts
@@ -0,0 +1,48 @@
+import { Dispatch } from 'react';
+import { customAxios } from './customAxios';
+import { DataState, Blog, DataAction } from '../reducers/dataReducer';
+
+export const useDataService = (state: DataState, dispatch: Dispatch<DataAction>) => {
+    const getProfile = () => {
+        customAxios.get('/profile')
+        .then(res => {
+            dispatch({
+                type: 'FETCH_PROFILE',
+                payload: res.data
+            })
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+    const getPosts = () => {
+        customAxios.get('/posts')
+        .then(res => {
+            dispatch({
+                type: 'FETCH_POSTS',
+                payload: res.data
+            })
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    };
+    const createNewPost = (blog: string) => {
+        customAxios
+        .post('/posts', blog)
+        .then(res => {
+            dispatch({
+                type: 'ADD_POST',
+                payload: res.data
+            })
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    };
+    return {
+        getPosts,
+        getProfile,
+        createNewPost
+    };
+};
