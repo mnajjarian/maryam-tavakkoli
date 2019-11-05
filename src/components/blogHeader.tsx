e34fc4a8acf2b4d8af366fe94ae31bd8547f639c
Mahdi Najjarian <mnajarian@gmail.com>
2019-11-05 17:21:26 +0200

    serve from backend

--- src/components/blogHeader.tsx
+++ src/components/blogHeader.tsx
@@ -6,34 +6,39 @@ interface Paragraph {
   text: string[];
 }
 interface Props {
-  posts: {
+  post: {
     id: string;
-    title: string;
-    paragraph: Paragraph[];
-    imgUrl: string;
+    content: string;
     createdAt: string;
   };
 }
 
 const BlogHeader = (props: Props) => {
-  const {
-    posts: { title, paragraph, imgUrl, createdAt }
-  } = props;
+  if(!props.post) {
+    return <div></div>
+  }
+  const {post, post: {content} } = props;
+  const draft = JSON.parse(content);
+  const { blocks } = draft;
+
+  const title = blocks.filter((b: any) => b.type === 'header-two')
+  const p = blocks.filter((b: any) => b.type === 'unstyled')
+
   return (
     <div className="blog__header">
-      <div className="blog__header__content">
+              <div className="blog__header__content">
         <div className="blog__header__items">
-          <h2>{title}</h2>
-          <p>{paragraph[0].text[0].substring(0, 320)}...</p>
-          <span>
+          <h2>{title[0].text}</h2>
+        <p>{p[0].text.substring(0, 320)}...</p>
+             <span>
             {new Intl.DateTimeFormat("en-us", {
               year: "numeric",
               month: "short",
               day: "2-digit"
-            }).format(new Date(createdAt))}
-          </span>
+            }).format(new Date(post.createdAt))}
+          </span> 
         </div>
-        <img src={require(`../${imgUrl}`)} alt="tech" />
+       {/*  <img src={require(`../${imgUrl}`)} alt="tech" /> */}
       </div>
     </div>
   );
