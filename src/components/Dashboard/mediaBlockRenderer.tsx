eb783291e37462da6c82d9842c9b87b5f7246890
mnajjarian <mnajarian@gmail.com>
2019-11-07 16:25:58 +0200

    add mediaBlocks

---
+++ src/components/Dashboard/mediaBlockRenderer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ContentBlock } from 'draft-js';
+import Media from './Media';
+
+export const mediaBlockRenderer = (block: ContentBlock) => {
+    if(block.getType() === 'atomic') {
+        return {
+            component: Media,
+            editable: false
+        }
+    }
+    return null;
+}
