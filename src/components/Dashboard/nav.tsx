7f1a4aec313bc51ff801383d523393086f9d3f7e
mnajjarian <mnajarian@gmail.com>
2019-11-12 10:13:53 +0200

    add logout and redirect

--- src/components/Dashboard/nav.tsx
+++ src/components/Dashboard/nav.tsx
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import classNames from "classnames";
 import { Link, Route } from "react-router-dom";
 import Profile from "./Profile";
 import Gallery from "../Gallery";
+import { AuthContext } from "../../contexts/authContext";
 
 const Nav = () => {
   const [toggle, setToggle] = useState(false);
+  const { authService } = useContext(AuthContext);
   const handleToggle = () => setToggle(!toggle);
-
+  const handleLogout = () => {
+    authService.logout();
+    
+  };
   return (
     <div className="nav">
       <ul className="nav__list">
@@ -35,7 +40,7 @@ const Nav = () => {
               "nav__menu-hide": !toggle
             })}
           >
-            <li className="nav__menu__item">Logout</li>
+            <li className="nav__menu__item" onClick={handleLogout} >Logout</li>
           </ul>
         </div>
       </ul>
