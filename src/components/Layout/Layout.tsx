e5255907b206b78e8914c69c4d4a7d67a33ab7b5
Mahdi Najjarian <mnajarian@gmail.com>
2020-06-07 10:37:56 +0300

    add scroll button

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,14 +1,31 @@
 import React, { ReactNode } from 'react'
 import { Nav } from '../Navbar/Navbar'
 import { Footer } from '../Footer/Footer'
+import { useScroll } from 'custom-hooks/useScroll'
 
 type Props = {
   children: ReactNode
 }
-export const Layout = ({ children }: Props): JSX.Element => (
-  <div className="container--fluid">
-    <Nav />
-    {children}
-    <Footer />
-  </div>
-)
+export const Layout = ({ children }: Props): JSX.Element => {
+  const handleScroll = (): void => window.scrollTo(0, 0)
+
+  const scroll = useScroll()
+  return (
+    <div
+      className="container--fluid"
+      style={{
+        position: 'relative',
+      }}
+    >
+      <Nav />
+      {children}
+      {scroll > 400 && (
+        <div className="scroll" onClick={handleScroll}>
+          <div></div>
+          <div></div>
+        </div>
+      )}
+      <Footer />
+    </div>
+  )
+}
